bec6fdd424fbd7cbcd77bee636710e284cf97eaf
Xalla <danielweber1992@outlook.de>
2020-06-23 21:36:02 +0200

    navigation changes / folder changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.scss';
-import { DataList } from './components/helpers/DataList';
+import { DataList } from './shared/helpers/DataList';
 import ImageList from "./components/image-list/ImageList";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
     const imageListData = DataList ? DataList : [];
@@ -11,6 +12,9 @@ function App() {
         <header>
             <h1>React Image List App</h1>
         </header>
+
+        <Navbar />
+
         <div className="App-Content">
             <ImageList props={imageListData} />
         </div>
---
+++ src/shared/services/http.service.js
@@ -0,0 +1,3 @@
+export const HttpService = () => {
+    // ... http service here (fatchAPI or axios most likely)
+}
